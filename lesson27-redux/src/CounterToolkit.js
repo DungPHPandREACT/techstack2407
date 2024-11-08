f2a62943bc66e97ef6a4898ece03534ac387bf53
DungPHPAndReact <dung2000cn@gmail.com>
2024-11-08 21:40:30 +0700

    setup middleware redux toolkit

--- lesson27-redux/src/CounterToolkit.js
+++ lesson27-redux/src/CounterToolkit.js
@@ -1,9 +1,19 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { decrement, increment, incrementByAmount } from './slices/counterSlice';
+import {
+	decrement,
+	fetchStudentValue,
+	increment,
+	incrementByAmount,
+} from './slices/counterSlice';
+import { useEffect } from 'react';
 
 const CounterToolkit = () => {
 	const count = useSelector((state) => state.counter.count);
 	const dispatch = useDispatch();
+
+	const data = useSelector((state) => state.counter.data);
+	const status = useSelector((state) => state.counter.status);
+	const error = useSelector((state) => state.counter.error);
 
 	const handleIncrement = () => {
 		dispatch(increment());
@@ -16,6 +26,16 @@ const CounterToolkit = () => {
 	const handleIncrementByAmount = () => {
 		dispatch(incrementByAmount(5));
 	};
+
+	useEffect(() => {
+		if (status === 'idle') {
+			dispatch(fetchStudentValue());
+		}
+	}, [status, dispatch]);
+
+	console.log('data: ', data);
+	console.log('status: ', status);
+	console.log('error: ', error);
 
 	return (
 		<div>
--- lesson27-redux/src/slices/counterSlice.js
+++ lesson27-redux/src/slices/counterSlice.js
@@ -1,8 +1,19 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const fetchStudentValue = createAsyncThunk(
+	'counter/fetchValue',
+	async () => {
+		const response = await fetch(
+			'https://6680276e56c2c76b495b50ad.mockapi.io/api/v1/students'
+		);
+
+		return response.json();
+	}
+);
 
 const counterSlice = createSlice({
 	name: 'counter',
-	initialState: { count: 0 },
+	initialState: { count: 0, data: [], status: 'idle', error: null },
 	reducers: {
 		increment: (state) => {
 			state.count += 1;
@@ -13,6 +24,20 @@ const counterSlice = createSlice({
 		incrementByAmount: (state, action) => {
 			state.count += action.payload;
 		},
+	},
+	extraReducers: (builder) => {
+		builder
+			.addCase(fetchStudentValue.pending, (state) => {
+				state.status = 'loading';
+			})
+			.addCase(fetchStudentValue.fulfilled, (state, action) => {
+				state.status = 'succeeded';
+				state.data = action.payload;
+			})
+			.addCase(fetchStudentValue.rejected, (state, action) => {
+				state.status = 'failed';
+				state.error = action.error.message;
+			});
 	},
 });
 
